e540a93074d734e56b29cb295b287e366610d047
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2026-01-22 09:49:34 +0100

    OpenAI client setup

--- src/magnet_code/client/llm_client.py
+++ src/magnet_code/client/llm_client.py
@@ -1,1 +1,22 @@
-print("Hello world!")
+from openai import AsyncOpenAI
+
+import os
+
+class LLMClient:
+    def __init__(self) -> None:
+        self._client : AsyncOpenAI | None = None
+
+    def get_client(self) -> AsyncOpenAI:
+        # If client is not created, create a new client
+        if self._client is None:
+            self._client = AsyncOpenAI(
+                api_key=os.environ.get('OPENAI_API_KEY'),
+                base_url=os.environ.get('OPENAI_API_URL'),
+            )
+        return self._client
+            
+    async def close(self) -> None:
+        """Close the client"""
+        if self._client:
+            await self._client.close()
+            self._client = None
