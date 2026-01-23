cbba36c80027a88be210ee89708f0b981c64022e
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2026-01-23 16:08:56 +0100

    Most basic agentic loop

--- src/magnet_code/agent/agent.py
+++ src/magnet_code/agent/agent.py
@@ -2,13 +2,18 @@ from typing import AsyncGenerator
 
 from magnet_code.agent.events import AgentEvent
 from magnet_code.client.llm_client import LLMClient
-from magnet_code.client.response import EventType
+from magnet_code.client.response import EventType, StreamEventType
 
 
 class Agent:
     def __init__(self):
         # Create a new client for this agent
         self.client = LLMClient()
+
+    async def run(self, message: str):
+        # Communicate back that the agent has started
+        yield AgentEvent.agent_start(message)
+        
 
     async def _agentic_loop(self) -> AsyncGenerator[AgentEvent, None]:
         """
@@ -18,4 +23,10 @@ class Agent:
         """
         messages = [{"role": "user", "content": "What's up"}]
         async for event in self.client.chat_completion(messages, True):
-            if event.type == EventType
+            # If the stream event is a text delta
+            if event.type == StreamEventType.TEXT_DELTA:
+                # We convert it to the delta agent event
+                content = event.text_delta.content
+                yield AgentEvent.text_delta(content)
+            elif event.type == StreamEventType.ERROR:
+                yield AgentEvent.agent_error(event.error or "Unknown error occurred.")
