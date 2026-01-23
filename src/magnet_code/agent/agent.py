8f56677dca8122ff61c6d4ddad0761df8e0d157a
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2026-01-23 16:15:26 +0100

    Yielding the full response

--- src/magnet_code/agent/agent.py
+++ src/magnet_code/agent/agent.py
@@ -13,7 +13,16 @@ class Agent:
     async def run(self, message: str):
         # Communicate back that the agent has started
         yield AgentEvent.agent_start(message)
+        # Add:
+        #   user message to context
+        #   agent hooks that could run
         
+        # Run the main agentic loop
+        async for event in self._agentic_loop():
+            yield event
+        
+        # After the loop completed, communicate it with a final end event
+        yield AgentEvent.agent_end()
 
     async def _agentic_loop(self) -> AsyncGenerator[AgentEvent, None]:
         """
@@ -22,11 +31,18 @@ class Agent:
         - context management (coming soon)
         """
         messages = [{"role": "user", "content": "What's up"}]
+        # Initial variable where we accumulate the response
+        response_text = ""
+        
         async for event in self.client.chat_completion(messages, True):
             # If the stream event is a text delta
             if event.type == StreamEventType.TEXT_DELTA:
                 # We convert it to the delta agent event
                 content = event.text_delta.content
+                response_text += content
                 yield AgentEvent.text_delta(content)
             elif event.type == StreamEventType.ERROR:
                 yield AgentEvent.agent_error(event.error or "Unknown error occurred.")
+
+        if response_text:
+            yield AgentEvent.text_complete(response_text)
